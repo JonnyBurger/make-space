2be4b85168ab9bacf8bda66e0df461a15b489288
Jonny Burger <jonathanburger11@gmail.com>
2018-07-10 23:37:50 +0200

    Add test for strategy structure

--- test.js
+++ test.js
@@ -6,3 +6,12 @@ test('All strategies should have a unique key', t => {
 	const keys = Object.keys(strategies);
 	t.is(keys.length, uniq(keys).length);
 });
+
+test('All strategies should have same structure', t => {
+	const keys = Object.keys(strategies);
+	keys.forEach(k => {
+		t.true(typeof strategies[k].name === 'string');
+		t.true(typeof strategies[k].probe === 'function');
+		t.true(typeof strategies[k].command === 'string');
+	});
+});
