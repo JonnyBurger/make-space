70ef0585a1ad0807589b9850df3d6e8cb8a91bd9
Jonny Burger <jonathanburger11@gmail.com>
2018-07-10 23:35:31 +0200

    After effects cache strategy

--- src/strategies/after-effects-caches.js
+++ src/strategies/after-effects-caches.js
@@ -1,2 +1,10 @@
 const os = require('os');
 const getFolderSize = require('../helpers/get-folder-size');
+
+module.exports = {
+	name: 'Clear After Effects disk cache',
+	key: 'after-effects-caches',
+	command: 'rm -rfv ~/Library/Caches/Adobe/After Effects',
+	probe: () =>
+		getFolderSize(`${os.homedir()}/Library/Caches/Adobe/After Effects`)
+};
