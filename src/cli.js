d23bcfc0dae79b24291487186ca3ca94f74f34de
Jonny Burger <jonathanburger11@gmail.com>
2018-07-10 14:40:31 +0200

    Webpack before publishing

--- src/cli.js
+++ src/cli.js
@@ -1,22 +1,6 @@
-#!/usr/bin/env node
 'use strict';
-const meow = require('meow');
 const freeSpace = require('.');
 
-const cli = meow(`
-	Usage
-	  $ make-space [input]
-
-	Options
-	  --foo  Lorem ipsum [Default: false]
-
-	Examples
-	  $ make-space
-	  unicorns & rainbows
-	  $ make-space ponies
-	  ponies & rainbows
-`);
-
-freeSpace(cli.input[0] || 'unicorns')
+freeSpace()
 	.then(() => {})
 	.catch(err => console.log(err));
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+	entry: './dist/cli.js',
+	output: {
+		path: path.resolve(__dirname, 'bundled'),
+		filename: 'bundle.js'
+	},
+	target: 'node',
+	mode: 'production'
+};
